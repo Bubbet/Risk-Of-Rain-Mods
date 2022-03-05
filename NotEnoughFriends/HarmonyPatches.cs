037296979fc44ff3ffb13e5a3e81cd936a2fa357
Bubbet <brett1057228@gmail.com>
2022-03-04 22:20:32 -0600

    Fixed mod not working.

---
+++ NotEnoughFriends/HarmonyPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RoR2;
+using UnityEngine;
+
+namespace NotEnoughFriends
+{
+	[HarmonyPatch]
+	public static class HarmonyPatches
+	{
+		[HarmonyPrefix, HarmonyPatch(typeof(PlatformSystems), nameof(PlatformSystems.InitNetworkManagerSystem))]
+		public static void InitNetworkManagerSystem(GameObject networkManagerPrefabObject)
+		{
+			networkManagerPrefabObject.GetComponent<NetworkManagerConfiguration>().MaxConnections = NotEnoughFriendsPlugin.LobbySize.Value;
+		}
+	}
+}
--- NotEnoughFriends/NotEnoughFriendsPlugin.cs
+++ NotEnoughFriends/NotEnoughFriendsPlugin.cs
@@ -3,6 +3,7 @@ using System.Security;
 using System.Security.Permissions;
 using BepInEx;
 using BepInEx.Configuration;
+using HarmonyLib;
 using RoR2;
 using Console = RoR2.Console;
 
@@ -13,26 +14,47 @@ using Console = RoR2.Console;
 
 namespace NotEnoughFriends
 {
-	[BepInPlugin("bubbet.notenoughfriends", "Not Enough Friends", "1.0.0")]
+	[BepInPlugin("bubbet.notenoughfriends", "Not Enough Friends", "1.0.1")]
 	public class NotEnoughFriendsPlugin : BaseUnityPlugin
 	{
-		public ConfigEntry<int> LobbySize;
+		public static ConfigEntry<int> LobbySize;
 		public static (int players, int hardPlayers, int localPlayers) MaxDefault = (RoR2Application.maxPlayers, RoR2Application.hardMaxPlayers, RoR2Application.maxLocalPlayers);
+
+
+		private Harmony harmony;
+		private PatchClassProcessor patchProcessor;
+		private bool _enabled;
+
+		private void Awake()
+		{
+			harmony = new Harmony(Info.Metadata.GUID);
+			patchProcessor = new PatchClassProcessor(harmony, typeof(HarmonyPatches));
+			OnEnable();
+		}
+
 		private void OnEnable()
 		{
+			if (_enabled) return;
+			_enabled = true;
 			LobbySize = Config.Bind("General", "Lobby Size", 16, "Sets the max size of game lobbies.");
 			LobbySize.SettingChanged += LobbySizeOnSettingChanged;
 			RoR2Application.onLoad += OnGameLoad;
+			patchProcessor.Patch();
+			Logger.LogInfo("Enabling Mod");
 		}
 		private void OnDisable()
 		{
+			_enabled = false;
 			LobbySize.SettingChanged -= LobbySizeOnSettingChanged;
 			LobbySize = null;
 			RoR2Application.onLoad -= OnGameLoad;
 			SetSize();
+			harmony.UnpatchSelf();
+			Logger.LogInfo("Disabling Mod");
 		}
 		public void SetSize(int? maxPlayers = null, int? hardMaxPlayers = null, int? maxLocalPlayers = null)
 		{
+			Logger.LogMessage($"Setting max players to {maxPlayers ?? MaxDefault.players}");
 			RoR2Application.maxPlayers = maxPlayers ?? MaxDefault.players;
 			RoR2Application.hardMaxPlayers = hardMaxPlayers ?? MaxDefault.hardPlayers;
 			RoR2Application.maxLocalPlayers = maxLocalPlayers ?? MaxDefault.localPlayers;
