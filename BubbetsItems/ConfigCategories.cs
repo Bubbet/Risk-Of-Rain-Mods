84753d3256fe6bc09ed1be99405676fd488dd750
Bubbet <brett1057228@gmail.com>
2022-03-27 12:46:16 -0600

    throw out old entries when adding a new one

--- BubbetsItems/ConfigCategories.cs
+++ BubbetsItems/ConfigCategories.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Configuration;
+﻿using System.Linq;
+using BepInEx.Configuration;
 
 namespace BubbetsItems
 {
@@ -20,11 +21,14 @@ namespace BubbetsItems
 		public static ConfigEntry<T> Bind<T>(this ConfigFile file, ConfigCategoriesEnum which, string key, T defaultValue, string description, T? oldDefault = default)
 		{
 			var configEntry = file.Bind(Categories[(int) which], key, defaultValue, description);
-			var sharedEntry = file.Bind("Do Not Touch", "Config Defaults Reset", "", "Stores the old defaults that have been reset so they wont be again. Its not going to break anything if you do for some reason need to touch this, its job is just to prevent the config from resetting multiple times if you change the value back to the old default.");
-			
-			if (oldDefault == null || sharedEntry.Value.Contains(configEntry.Definition.Key + ": " + oldDefault) || !Equals(configEntry.Value, (T) oldDefault)) return configEntry;
-			configEntry.Value = (T) configEntry.DefaultValue;
-			sharedEntry.Value += configEntry.Definition.Key + ": " + oldDefault + "; ";
+			var sharedEntry = file.Bind("Do Not Touch", "Config Defaults Reset", "",
+				"Stores the old defaults that have been reset so they wont be again. Its not going to break anything if you do for some reason need to touch this, its job is just to prevent the config from resetting multiple times if you change the value back to the old default.");
+			if (oldDefault != null && !sharedEntry.Value.Contains(configEntry.Definition.Key + ": " + oldDefault) && Equals(configEntry.Value, (T) oldDefault))
+			{
+				configEntry.Value = (T) configEntry.DefaultValue;
+				var entries = sharedEntry.Value.Split(';').Where(x => !x.StartsWith(configEntry.Definition.Key)).ToArray();
+				sharedEntry.Value = string.Join(";", entries) +  configEntry.Definition.Key + ": " + oldDefault + "; ";
+			}
 
 			return configEntry;
 		}
