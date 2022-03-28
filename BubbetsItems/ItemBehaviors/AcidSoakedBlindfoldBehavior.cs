562805436ab98ee51f1ca76b028c487a48af5399
Bubbet <brett1057228@gmail.com>
2022-03-28 11:51:05 -0600

    Add AcidSoakedBlindfold

---
+++ BubbetsItems/ItemBehaviors/AcidSoakedBlindfoldBehavior.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using BubbetsItems.Items;
+using RoR2;
+using RoR2.Items;
+using UnityEngine;
+
+namespace BubbetsItems.ItemBehaviors
+{
+	public class AcidSoakedBlindfoldBehavior : BaseItemBodyBehavior
+	{
+		private float _deployableTime;
+		private const float TimeBetweenRespawns = 30f;
+		private const float TimeBetweenRetries = 1f;
+		private const DeployableSlot Slot = (DeployableSlot) 340502;
+		
+		[ItemDefAssociation(useOnServer = true, useOnClient = false)]
+		private static ItemDef GetItemDef()
+		{
+			return AcidSoakedBlindfold.instance.ItemDef;
+		}
+
+		private void FixedUpdate()
+		{
+			var master = body.master; 
+			if (!master) return;
+			var maxCount = AcidSoakedBlindfold.instance.scalingInfos[0].ScalingFunction(stack);
+			var count = master.GetDeployableCount(Slot);
+			if (count >= maxCount) return;
+			_deployableTime -= Time.fixedDeltaTime;
+			if (_deployableTime > 0) return;
+
+			var request = new DirectorSpawnRequest(
+				LegacyResourcesAPI.Load<SpawnCard>("SpawnCards/CharacterSpawnCards/cscVermin"),
+				new DirectorPlacementRule
+				{
+					placementMode = DirectorPlacementRule.PlacementMode.Approximate,
+					minDistance = 3f,
+					maxDistance = 40f,
+					spawnOnTarget = transform
+				}, RoR2Application.rng
+			) {summonerBodyObject = gameObject, onSpawnedServer = VerminSpawnedServer};
+			DirectorCore.instance.TrySpawnObject(request);
+			_deployableTime = master.GetDeployableCount(Slot) >= maxCount ? TimeBetweenRetries : TimeBetweenRespawns;
+		}
+
+		private void VerminSpawnedServer(SpawnCard.SpawnResult obj)
+		{
+			var instance = obj.spawnedInstance;
+			if (!instance) return;
+			var master = instance.GetComponent<CharacterMaster>();
+			if (!master) return;
+			master.teamIndex = body.master.teamIndex;
+			//master.inventory;
+
+			var greenChance = AcidSoakedBlindfold.instance.scalingInfos[2].ScalingFunction(stack);
+
+			var list1 = Run.instance.availableTier1DropList;
+			var list2 = Run.instance.availableTier2DropList;
+			
+			for (var i = 0; i < AcidSoakedBlindfold.instance.scalingInfos[1].ScalingFunction(stack); i++)
+			{
+				master.inventory.GiveItem(Run.instance.treasureRng.nextNormalizedFloat < greenChance
+					? list2[Run.instance.treasureRng.RangeInt(0, list2.Count)].pickupDef.itemIndex
+					: list1[Run.instance.treasureRng.RangeInt(0, list1.Count)].pickupDef.itemIndex);
+			}
+
+			var deployable = instance.AddComponent<Deployable>();
+			if (!deployable) return;
+			deployable.ownerMaster = body.master;
+			body.master.AddDeployable(deployable, Slot);
+		}
+	}
+}
---
+++ BubbetsItems/Items/AcidSoakedBlindfold.cs
@@ -0,0 +1,28 @@
+﻿namespace BubbetsItems.Items
+{
+	public class AcidSoakedBlindfold : ItemBase
+	{
+		public static AcidSoakedBlindfold instance;
+		protected override void MakeConfigs()
+		{
+			base.MakeConfigs();
+			AddScalingFunction("1", "Vermin Count");
+			AddScalingFunction("[a] * 5 + 5", "Item Count");
+			AddScalingFunction("0.2", "Green Item Chance"); 
+		}
+
+		protected override void MakeTokens()
+		{
+			base.MakeTokens();
+			AddToken("ACIDSOAKEDBLINDFOLD_NAME", "Acid Soaked Blindfold");
+			AddToken("ACIDSOAKEDBLINDFOLD_DESC", "Gain {0} blind vermin ally with {1} green or white items.");
+			AddToken("ACIDSOAKEDBLINDFOLD_PICKUP", "Gain a blind vermin ally.");
+			AddToken("ACIDSOAKEDBLINDFOLD_LORE", "");
+		}
+
+		public AcidSoakedBlindfold()
+		{
+			instance = this;
+		}
+	}
+}
