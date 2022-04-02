b390027c8c6c6e62e691fbe967d76d3826c66015
Bubbet <brett1057228@gmail.com>
2022-04-02 02:23:18 -0600

    Proc coefficient item test

---
+++ BubbetsItems/Items/ProcCoefficientIncrease.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using RoR2;
+
+namespace BubbetsItems.Items
+{
+	public class ProcCoefficientIncrease : ItemBase
+	{
+		public static ProcCoefficientIncrease Instance;
+
+		public ProcCoefficientIncrease()
+		{
+			Instance = this;
+		}
+
+		protected override void MakeConfigs()
+		{
+			base.MakeConfigs();
+			AddScalingFunction("1 + [a] * 0.3", "Proc Coefficient");
+		}
+
+		[HarmonyPrefix, HarmonyPatch(typeof(HealthComponent), nameof(HealthComponent.TakeDamage))]
+		public static void ApplyCoefficent(HealthComponent __instance, ref DamageInfo damageInfo)
+		{
+			var count = damageInfo.attacker?.GetComponent<CharacterBody>()?.inventory?.GetItemCount(Instance.ItemDef) ?? 0;
+			if (count <= 0) return;
+			damageInfo.procCoefficient *= Instance.scalingInfos[0].ScalingFunction(count);
+		}
+	}
+}
