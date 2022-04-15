3aa434c7cfa54463d6ccdf7a3792e222b32e1695
Bubbet <brett1057228@gmail.com>
2022-04-15 13:21:00 -0600

    Only your damage numbers

---
+++ OnlyYourDamageNumbers/HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using UnityEngine;
+
+namespace OnlyYourDamageNumbers
+{
+	[HarmonyPatch]
+	public static class HarmonyPatches
+	{
+		[HarmonyILManipulator, HarmonyPatch(typeof(HealthComponent), nameof(HealthComponent.HandleDamageDealt))]
+		public static void FixDamageDealt(ILContext il)
+		{
+			var c = new ILCursor(il) {Index = il.Instrs.Count - 1};
+			var where = c.Next;
+			c.GotoPrev(x => x.MatchCall(typeof(DamageNumberManager),"get_instance"));
+			
+			c.Emit(OpCodes.Ldloc_0);
+			c.EmitDelegate<Func<DamageDealtMessage, bool>>(message => message.attacker == LocalUserManager.readOnlyLocalUsersList[0].cachedBodyObject);
+			c.Emit(OpCodes.Brfalse, where);
+		}
+		
+		[HarmonyILManipulator, HarmonyPatch(typeof(HealthComponent), nameof(HealthComponent.HandleHeal))]
+		public static void FixHeal(ILContext il)
+		{
+			var c = new ILCursor(il) {Index = il.Instrs.Count - 1};
+			var where = c.Next;
+			c.GotoPrev(x => x.MatchCall(typeof(DamageNumberManager),"get_instance")); // c.GotoPrev(x => x.MatchCall(typeof(DamageNumberManager).GetProperty("instance", BindingFlags.Public | BindingFlags.Static)?.GetGetMethod()));
+			c.Emit(OpCodes.Ldloc_0);
+			c.EmitDelegate<Func<HealthComponent.HealMessage, bool>>(message => message.target == LocalUserManager.readOnlyLocalUsersList[0].cachedBodyObject);
+			c.Emit(OpCodes.Brfalse, where);
+		}
+	}
+}
---
+++ OnlyYourDamageNumbers/OnlyYourDamageNumbersPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using HarmonyLib;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: UnverifiableCode]
+
+namespace OnlyYourDamageNumbers
+{
+	[BepInPlugin("bubbet.onlyyourDamagenumbers", "Only Your Damage Numbers", "1.0.0")]
+	public class OnlyYourDamageNumbersPlugin : BaseUnityPlugin
+	{
+		public void Awake()
+		{
+			var harm = new Harmony(Info.Metadata.GUID);
+			new PatchClassProcessor(harm, typeof(HarmonyPatches)).Patch();
+		}
+	}
+}
