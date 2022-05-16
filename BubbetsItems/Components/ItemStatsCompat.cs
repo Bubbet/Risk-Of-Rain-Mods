1e83bd50547759737c55a3cbb1ed230559b274df
Bubbet <brett1057228@gmail.com>
2022-05-16 06:52:32 -0600

    Initial ItemStatsCompat

---
+++ BubbetsItems/Components/ItemStatsCompat.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using BepInEx.Bootstrap;
+
+namespace BubbetsItems
+{
+	public static class ItemStatsCompat
+	{
+		public static bool IsEnabled => Chainloader.PluginInfos.ContainsKey("com.Moffein.ItemStats");
+		public static void Init()
+		{
+			if (!IsEnabled) return;
+			ModIsEnabledInit();
+		}
+
+		private static void ModIsEnabledInit()
+		{
+			ItemStats.ItemStats.IgnoredItems.AddRange(ItemBase.Items.Select(x => x.ItemDef));
+		}
+	}
+}
