e2d1810f7d9009536f0bdec4d3026d02945f6b1c
Bubbet <brett1057228@gmail.com>
2023-08-11 21:53:03 -0600

    remove item mod in prep for subtree

--- BubbetsItems/Helpers/ScalingInfoIndexer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace BubbetsItems.Helpers;
-
-public class ScalingInfoIndexer : List<ItemBase.ScalingInfo>
-{
-    public ItemBase.ScalingInfo this[string which] => this.First(x => x._name == which);
-}
--- BubbetsItems/zipper.bat
+++
@@ -1,5 +0,0 @@
-﻿
-for /f "delims=" %%A in ('cd') do ( set WHERE=%%~nxA )
-set WHERE=%WHERE: =%
-echo %WHERE%
-"D:\Program Files\7-Zip\7z.exe" a -y -tzip %WHERE%.zip "%cd%\bin\Debug\netstandard2.0\%WHERE%.dll" "%cd%\README.md" "%cd%\icon.png" "%cd%\manifest.json" "%cd%\BuiltObjects\mainassetbundle" "%cd%\NCalc.dll" "%cd%\BuiltObjects\GeneratedSoundBanks\Windows\BubbetsItems.bnk"  
